8fbf12c37ba53f87306c7985168e6bf4185f01ee
nicolas-o <nicolas.p.orellana@gmail.com>
2021-04-23 10:52:01 -0400

    Changed function and variable names inside index.js

--- charCounter/js/index.js
+++ charCounter/js/index.js
@@ -1,14 +1,14 @@
 async function writeInDOM(params) {
   let CountCharacters = await params;
   let character = CountCharacters[0];
-  let characterCounter = CountCharacters[1];
+  let Counter = CountCharacters[1];
   let source = CountCharacters[2];
 
   // Selecciona la sección con la clase .charCounter.
   let charCounterContainer = document.querySelector(".charCounter");
 
   // Añade al HTML el nombre del caracter y cuantas veces aparece.
-  charCounterContainer.innerHTML += `<h3>La letra "${character}" aparece ${characterCounter} veces en los nombres de ${source.toUpperCase()} </h3>`;
+  charCounterContainer.innerHTML += `<h3>La letra "${character}" aparece ${Counter} veces en los nombres de ${source.toUpperCase()} </h3>`;
 }
 
 console.time("charCounter Timer");
