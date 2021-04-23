fdcbc980bd1e604be3d5da5edcefe8c216cabc8f
nicolas-o <nicolas.p.orellana@gmail.com>
2021-04-23 13:26:09 -0400

    Added a test to episodeLocations

--- test/episodeLocationsTest.js
+++ test/episodeLocationsTest.js
@@ -1,7 +1,7 @@
 const expect = require("chai").expect;
 const getData = require("../episodeLocations/js/getData");
 
-//Testea si getData retorna la misma cantidad de objetos solicitados.
+//Testea si getData() retorna la misma cantidad de objetos solicitados.
 describe("getData", () => {
   it("Tiene la misma cantidad de objetos", async () => {
     const result = await getData("episode");
