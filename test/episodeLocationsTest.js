e49952e5dbc185e136cc1d54bff2f28e3e1bed84
nicolas-o <nicolas.p.orellana@gmail.com>
2021-04-23 13:11:57 -0400

    Added a test or episodeLocations

---
+++ test/episodeLocationsTest.js
@@ -0,0 +1,14 @@
+const expect = require("chai").expect;
+const getData = require("../episodeLocations/js/getData");
+
+//Testea si getData retorna la misma cantidad de objetos solicitados.
+describe("getData", () => {
+  it("Tiene la misma cantidad de objetos", async () => {
+    const result = await getData("episode");
+    expect(result.length).deep.to.equal(41);
+  });
+  it("Tiene la misma cantidad de objetos", async () => {
+    const result = await getData("character");
+    expect(result.length).deep.to.equal(671);
+  });
+});
